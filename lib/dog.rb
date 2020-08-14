b74669f9839cf3cdb7e3f3134b0e0d39e5ccce97
Gabriela johnson <johnsongabriela12@gmail.com>
2020-08-14 22:17:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -51,7 +51,15 @@ class Dog
         self.new(id: row[0], name: row[1], breed: row[2])
     end 
     
-    
+    def self.find_by_id
+      sql = <<-SQL
+      SELECT *
+      FROM dogs 
+      WHERE id = ?
+      SQL
+      
+      
+    end
     
     
   
